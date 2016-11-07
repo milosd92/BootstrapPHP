fc58a4c2bcb88b75098914b7f2062d58bf9ed482
milosd92 <milosdanilov@gmail.com>
2016-11-07 21:45:02 +0100

    adding basic support for glyphicons

---
+++ src/BootstrapPHP/Components/Glyphicon.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the BootstrapPHP package.
+ *
+ * (c) Milos Danilov <milosdanilov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BootstrapPHP\Components;
+
+/**
+ * Creates a Glyphicon
+ *
+ * @package BootstrapPHP\Components
+ * @author Milos Danilov <milosdanilov@gmail.com>
+ */
+class Glyphicon
+{
+    protected $glyphicon = '';
+
+    public function __construct($glyphicon)
+    {
+        $this->glyphicon = $glyphicon;
+    }
+
+    public function __toString()
+    {
+        return "<span class='glyphicon {$this->glyphicon}'></span>";
+    }
+}
