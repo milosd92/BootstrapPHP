98e8682a6aea6578ebdec88e644e53d8d6108f5f
milosd92 <milosdanilov@gmail.com>
2016-11-08 20:44:56 +0100

    adding class to create caret

---
+++ src/BootstrapPHP/Helpers/Caret.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the BootstrapPHP package.
+ *
+ * (c) Milos Danilov <milosdanilov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BootstrapPHP\Helpers;
+
+
+class Caret
+{
+    public function __toString()
+    {
+        return "<span class='caret'></span>";
+    }
+}
